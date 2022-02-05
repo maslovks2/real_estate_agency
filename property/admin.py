c25f0f50686b4c0a1ec6fc2cbbd9e95e5d756ee1
maslovks2 <87320716+maslovks2@users.noreply.github.com>
2022-02-05 14:06:12 +0300

    fill with data field Flat.construction_year

--- property/admin.py
+++ property/admin.py
@@ -1,3 +1,4 @@
+from csv import list_dialects
 from django.contrib import admin
 
 from .models import Flat
@@ -7,3 +8,5 @@ from .models import Flat
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('Flat_town', 'Flat_address', 'Flat_owner')
     readonly_fields = ('created_at',)
+    list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
+    list_editable = ('new_building',)
